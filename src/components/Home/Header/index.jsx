cf1e40a978afaaca4b9247195a661d4180fdc640
Juan Sebastian Galeano A <juan_seb.galeano@uao.edu.co>
2023-05-03 15:07:41 -0500

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Universidad Autónoma de Occidente</h2>
+    <h1>
+      <span>“</span> Monitopia  <span>”</span>
+    </h1>
+    <p>
+      Acesso fácil <br /> a un aprendizaje constante
+    </p>
+  </header>
+);
+
+export default Header;
